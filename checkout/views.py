5405707402b94c07f97847afab96b11006fd035d
ThijsTerporten <twpterporten@gmail.com>
2022-02-14 21:14:27 +0000

    Completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,4 @@
+"""View for checkout """
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
@@ -26,6 +27,7 @@ def cache_checkoput_data(request):
         return HttpResponse(content=0, status=400)
 
 def checkout(request):
+    """Fucntion for checking out """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -44,8 +46,12 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
-        if order_form.is_valid():
+        if order_form.is_valid(commit=False):
             order = order_form.save()
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
