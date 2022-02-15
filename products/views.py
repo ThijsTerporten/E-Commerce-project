4bfd8a6dff61ed25f290b6888ff4c3c1859e10b3
ThijsTerporten <twpterporten@gmail.com>
2022-02-15 19:12:13 +0000

    Finished add product functionality

--- products/views.py
+++ products/views.py
@@ -72,8 +72,17 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store"""
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Succesfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product, please ensure form is valid.')
+    else:
+        form = ProductForm()
 
-    form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
