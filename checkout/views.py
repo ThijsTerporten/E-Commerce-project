2d524811600fb6fcc9844d07a6b753f756cab5e6
ThijsTerporten <twpterporten@gmail.com>
2022-02-15 11:21:29 +0000

    Update checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -1,33 +1,34 @@
-"""View for checkout """
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
-import stripe
-import json
-from bag.contexts import bag_contents
 
-from products.models import Product
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+from products.models import Product
+from bag.contexts import bag_contents
+
+import stripe
+import json
 
 @require_POST
-def cache_checkoput_data(request):
+def cache_checkout_data(request):
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dump(request.session.get('bag', {})),
+            'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
-            'username': request.user
+            'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry paymenty cannot be processed')
-        return HttpResponse(content=0, status=400)
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
-    """Fucntion for checking out """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -46,8 +47,8 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
-        if order_form.is_valid(commit=False):
-            order = order_form.save()
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
             pid = request.POST.get('client_secret').split('_secret')[0]
             order.stripe_pid = pid
             order.original_bag = json.dumps(bag)
